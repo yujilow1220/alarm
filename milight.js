1c2a369cc5f3f5398a77e96c74340af06db87bad
Takuya Takahashi <info@yujilow.net>
2016-07-31 18:19:13 +0900

    modify milight

--- milight.js
+++ milight.js
@@ -7,9 +7,12 @@ var milight = new Milight({
 var i = 0;
 // All zones on
 milight.zone(1).on();
+milight.zone(1).brightness(i);
 setInterval(function(){
+  console.log('now brightness = ' + i);
   milight.zone(1).brightness(i, function(err){
+
     i ++;
     if(i === 100)process.exit(0);
-  }, 1000 * 60);
-});
+  });
+}, 1000);
