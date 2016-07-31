d756f362ecb24659e3babc7c319fb5673c737e3b
Takuya Takahashi <info@yujilow.net>
2016-07-31 16:34:02 +0900

    add function of delete and list

--- routes/alarm.js
+++ routes/alarm.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var alarm = require('../lib/alarm');
+alarm.start();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/list', function(req,res,next){
+  alarm.list(function(alarms){
+    res.send(alarms);
+  })
 });
 
 router.post('/create', function(req, res, next){
@@ -21,5 +28,12 @@ router.get('/stop', function(req, res, next){
   alarm.stop();
   res.send("ok");
 });
+
+router.delete('/:name', function(req, res, next){
+  alarm.delete(req.params['name'], function(){
+    alarm.reload();
+    res.send('ok');
+  })
+})
 
 module.exports = router;
