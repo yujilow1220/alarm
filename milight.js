03dbf4c58039aee1801dc43728463d0614946a44
Takuya Takahashi <info@yujilow.net>
2016-07-31 18:16:16 +0900

    add milight.js

---
+++ milight.js
@@ -0,0 +1,15 @@
+var Milight = require("milight");
+
+var milight = new Milight({
+    host: "192.168.0.255",
+    broadcast: true
+});
+var i = 0;
+// All zones on
+milight.zone(1).on();
+setInterval(function(){
+  milight.zone(1).brightness(i, function(err){
+    i ++;
+    if(i === 100)process.exit(0);
+  }, 1000 * 60);
+});
